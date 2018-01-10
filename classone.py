d3af1be9908d4f4f9c1459653f64f31ffcdb850b
afour <aniruddhacool09@gmail.com>
2018-01-10 16:40:48 +0530

    Creating Simple Class in Python

---
+++ classone.py
@@ -0,0 +1,20 @@
+
+
+#Creating Simple Class in Python and accessing the variables and displaying it.
+class classone:
+	var = 30
+
+	new_var = 'Python'
+	
+	def myfunc(self):
+		print("Inside the main function block")
+
+obj=classone() # Creating object of classone
+
+print(obj.var) 
+
+print(obj.new_var)
+
+print(obj.functionds)
+
+print(obj.__doc__)
